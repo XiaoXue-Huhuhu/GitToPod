5a35e6a0fe93e952abd6e61e73b995e32a20ad86
Xueyan Wang <202221130242@stu.zuel.edu.cn>
2025-05-10 19:22:27 +0800

    Update gemini_test.py

--- backend/app/services/gemini_test.py
+++ backend/app/services/gemini_test.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key='AIzaSyDmKaoRB4caVcFysxMlTvxofFtvVwV0fa4')  # 填入自己的api_key 
+genai.configure(api_key='your-api-key')  # 填入自己的api_key 
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content("告诉我太阳系中最大行星的相关知识")
 print(response.text)
